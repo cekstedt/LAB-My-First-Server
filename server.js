5baca9d9a1cedfc133225d5ade86146ca095b9da
Chris Ekstedt <9835700+cekstedt@users.noreply.github.com>
2022-09-14 14:09:34 -0700

    Add config.json and retouch sent html.

--- server.js
+++ server.js
@@ -1,23 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
 
 // Set what to serve when requests come in. "/" is root domain requests. (Eg: homepage, index.html, etc.)
 app.get("/", function(req, res) {
-  res.send("<h1>Hello world!</h1>");
+  res.send(
+    `<h1>Hello world!</h1>
+     <p><a href="/contact">contact me</a></p>
+     <p><a href="/about">about me</a></p>
+     <p><a href="/hobbies">my hobbies</a></p>
+     `
+  );
 });
 
 // Serve different content based on "/contact" route.
 app.get("/contact", function(req, res) {
-  res.send("Contact me at: example@gmail.com");
+  res.send("<p>Contact me at: example@gmail.com</p>");
 });
 
 app.get("/about", function(req, res) {
-  res.send('Ever seen a man eat his own weight in chili dogs?<br><img alt="robbie rotten would you like to" data-src="https://i.kym-cdn.com/entries/icons/mobile/000/036/654/Screen_Shot_2021-03-12_at_1.47.58_PM.jpg" src="https://i.kym-cdn.com/entries/icons/mobile/000/036/654/Screen_Shot_2021-03-12_at_1.47.58_PM.jpg" title="Would You Like To?">');
+  res.send(
+    `<h1>Ever seen a man eat his own weight in chili dogs?</h1>
+     <img alt="robbie rotten would you like to"
+      data-src="https://i.kym-cdn.com/entries/icons/mobile/000/036/654/Screen_Shot_2021-03-12_at_1.47.58_PM.jpg"
+      src="https://i.kym-cdn.com/entries/icons/mobile/000/036/654/Screen_Shot_2021-03-12_at_1.47.58_PM.jpg"
+      title="Would You Like To?">
+      `
+  );
 });
 
 app.get("/hobbies", function(req, res) {
-  res.send("Kicking bubble gum and eating ass.");
+  res.send("<p>Kicking bubble gum and eating ass.</p>");
 });
 
 // Listen for incoming requests on PORT. Callback function fires when listening begins.
